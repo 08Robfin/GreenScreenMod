ceed12f1f4c1b93562fba063d2c79996cc80583b
Robfin <47146792+08Robfin@users.noreply.github.com>
2024-03-26 18:44:49 +0100

    Update Plugin.cs

--- MonkeyHead/Plugin.cs
+++ MonkeyHead/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using GorillaExtensions;
 using System;
 using System.IO;
@@ -20,7 +20,15 @@ namespace MonkeyHead
     {
         bool inRoom;
 
-        GameObject thing;
+        static GameObject thing;
+
+        GameObject Blue,
+                   Green,
+                   Red,
+                   Enable,
+                   Grey;
+
+        GameObject pizza;
 
         void Start()
         {
@@ -30,7 +38,7 @@ namespace MonkeyHead
             Utilla.Events.GameInitialized += OnGameInitialized;
         }
 
-        void ChangeColour(Color colour)
+        public static void ChangeColour(Color colour)
         {
             thing.transform.GetChild(0).GetComponent<Renderer>().material.color = colour;
             thing.transform.GetChild(1).GetComponent<Renderer>().material.color = colour;
@@ -38,15 +46,33 @@ namespace MonkeyHead
         }
 
         bool isEnabled;
+        public static bool thingEnabled = true;
         void Update()
         {
             if (Keyboard.current.tabKey.wasPressedThisFrame) isEnabled = !isEnabled;
+            thing.SetActive(thingEnabled);
+            pizza.GetComponent<Renderer>().enabled = !thingEnabled;
         }
 
         void OnGUI()
         {
             if (isEnabled)
+
             {
+                if (GUI.Button(new Rect(25, 420, 250, 25), "Enable/Disable"))
+                {
+                    Debug.Log("wow you pressed a button");
+                    thingEnabled =!thingEnabled;
+
+                }
+
+                if (GUI.Button(new Rect(25, 370, 250, 25), "Grey"))
+                {
+                    Debug.Log("wow you pressed a button");
+                    ChangeColour(Color.grey);
+
+                }
+
                 if (GUI.Button(new Rect(25, 310, 250, 25), "Red"))
                 {
                     Debug.Log("wow you pressed a button");
@@ -68,7 +94,34 @@ namespace MonkeyHead
             }
         }
 
-      
+
+        GameObject CreateButan(Color color, String name, float x, float z)
+        {
+            GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            temp.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
+            temp.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            temp.transform.position = new Vector3(x, 16.8034f, z);
+            temp.transform.localRotation = Quaternion.Euler(0f, 30f, 0f);
+            temp.layer = 18;
+            temp.AddComponent<Butan>();
+            temp.GetComponent<Renderer>().material.color = color;
+            temp.name = name;
+            return temp;
+        }
+
+        GameObject CreateButan(Color color, String name, float x, float z, float y)
+        {
+            GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            temp.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
+            temp.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            temp.transform.position = new Vector3(x, y, z);
+            temp.transform.localRotation = Quaternion.Euler(0f, 30f, 0f);
+            temp.layer = 18;
+            temp.AddComponent<Butan>();
+            temp.GetComponent<Renderer>().material.color = color;
+            temp.name = name;
+            return temp;
+        }
 
 
         void OnGameInitialized(object sender, EventArgs e)
@@ -80,6 +133,17 @@ namespace MonkeyHead
             thing.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
             thing.transform.localRotation = Quaternion.Euler(0f, 210f, 0f);
 
+            Blue = CreateButan(Color.blue, "Blue", -56.3f, -120.2246f);
+
+            Green = CreateButan(Color.green, "Green", -56, -120.4046f);
+
+            Red = CreateButan(Color.red, "Red", -55.7f, -120.5746f);
+
+            Grey = CreateButan(Color.grey, "Grey", -55.4f, -120.7446f);
+
+            Enable = CreateButan(Color.white, "Enable", -56.3013f, -120.2246f, 17.1534f);
+
+            pizza = GameObject.Find("Environment Objects/LocalObjects_Prefab/City/CosmeticsRoomAnchor/ShoppingCenterAnchor/Stuff/Empty Stuff/table (1)");
         }
 
         public AssetBundle LoadAssetBundle(string path)
