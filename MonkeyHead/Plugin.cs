7353a5c3d170af7abb02b4312a905e894671b9b2
Robfin <47146792+08Robfin@users.noreply.github.com>
2024-03-26 19:00:27 +0100

    Update Plugin.cs

--- MonkeyHead/Plugin.cs
+++ MonkeyHead/Plugin.cs
@@ -9,16 +9,11 @@ using Utilla;
 
 namespace MonkeyHead
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool inRoom;
+        public static bool thingEnabled = true;
 
         static GameObject thing;
 
@@ -30,13 +25,7 @@ namespace MonkeyHead
 
         GameObject pizza;
 
-        void Start()
-        {
-            /* A lot of Gorilla Tag systems will not be set up when start is called /*
-			/* Put code in OnGameInitialized to avoid null references */
-
-            Utilla.Events.GameInitialized += OnGameInitialized;
-        }
+        void Start() => Events.GameInitialized += OnGameInitialized;
 
         public static void ChangeColour(Color colour)
         {
@@ -45,55 +34,11 @@ namespace MonkeyHead
             thing.transform.GetChild(2).GetComponent<Renderer>().material.color = colour;
         }
 
-        bool isEnabled;
-        public static bool thingEnabled = true;
         void Update()
         {
-            if (Keyboard.current.tabKey.wasPressedThisFrame) isEnabled = !isEnabled;
             thing.SetActive(thingEnabled);
             pizza.GetComponent<Renderer>().enabled = !thingEnabled;
         }
-
-        void OnGUI()
-        {
-            if (isEnabled)
-
-            {
-                if (GUI.Button(new Rect(25, 420, 250, 25), "Enable/Disable"))
-                {
-                    Debug.Log("wow you pressed a button");
-                    thingEnabled =!thingEnabled;
-
-                }
-
-                if (GUI.Button(new Rect(25, 370, 250, 25), "Grey"))
-                {
-                    Debug.Log("wow you pressed a button");
-                    ChangeColour(Color.grey);
-
-                }
-
-                if (GUI.Button(new Rect(25, 310, 250, 25), "Red"))
-                {
-                    Debug.Log("wow you pressed a button");
-                    ChangeColour(Color.red);
-
-                }
-                if (GUI.Button(new Rect(25, 280, 250, 25), "Green"))
-                {
-                    Debug.Log("wow you pressed a button");
-                    ChangeColour(Color.green);
-
-                }
-                if (GUI.Button(new Rect(25, 340, 250, 25), "Blue"))
-                {
-                    Debug.Log("wow you pressed a button");
-                    ChangeColour(Color.blue);
-
-                }
-            }
-        }
-
 
         GameObject CreateButan(Color color, String name, float x, float z)
         {
@@ -123,7 +68,6 @@ namespace MonkeyHead
             return temp;
         }
 
-
         void OnGameInitialized(object sender, EventArgs e)
         {
             var bundle = LoadAssetBundle("MonkeyHead.Assets.greenscreen");
@@ -148,13 +92,8 @@ namespace MonkeyHead
 
         public AssetBundle LoadAssetBundle(string path)
         {
-            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
-            stream.Close();
-            return bundle;
+            Assembly.GetExecutingAssembly().GetManifestResourceStream(path).Close();
+            return AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(path));
         }
-
-        
-        
     }
 }
